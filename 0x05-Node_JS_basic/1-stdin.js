6336187ebccffd5ddc2f9dbcea000129ee97a54b
Emmanuel Adinoyi Adaja <emmanueladaja@yahoo.com>
2024-02-05 00:53:26 -0800

    corrected lint errors

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,12 +1,13 @@
 // writes to stdout and receives input from stdin
 
 process.stdin.setEncoding('utf-8');
-console.log("Welcome to Holberton School, what is your name?");
-process.stdin.on("readable", () => {
+console.log('Welcome to Holberton School, what is your name?');
+process.stdin.on('readable', () => {
   const inp = process.stdin.read();
   console.log(`Your name is: ${inp}`);
+  process.stdin.end();
 });
 
-process.on("close", (code) => {
-    console.log('This important software is now closing');
+process.on('close', () => {
+  console.log('This important software is now closing');
 });
