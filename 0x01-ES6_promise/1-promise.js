7b424b74130edd033efaf517554260ef260123b6
Emmanuel Adinoyi Adaja <emmanueladaja@yahoo.com>
2024-06-12 17:40:03 +0100

    return a full promise

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,12 @@
+// Returns a full promise
+
+export default function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+   if (success) {
+    resolve({
+      status: 200,
+      body: 'Success'});
+  } else {
+    reject('The fake API is not working currently');
+  }});
+}
