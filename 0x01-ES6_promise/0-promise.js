7da6533d21922bfb6f4cef7a66968ed2f05b627f
Emmanuel Adinoyi Adaja <emmanueladaja@yahoo.com>
2024-06-12 04:46:45 +0100

    Working on promises

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,5 @@
+// Exports a function that returns a promise.
+
+export default function getResponseFromAPI() {
+    return new Promise((resolve, reject) => resolve());
+}
---
+++ 0x01-ES6_promise/README.md
@@ -0,0 +1,2 @@
+#ES6 PROMISES
+Introduction to promises in javascript.
