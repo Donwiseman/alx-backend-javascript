18fc668161aa505d85653f87cdbd1d17ed7de1d3
Emmanuel Adinoyi Adaja <emmanueladaja@yahoo.com>
2024-06-13 20:58:11 +0100

    returned the promise object then chain

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,7 +1,8 @@
 // handling promise
 
 export default function handleResponseFromAPI(promise) {
-  promise.then(() => ({ status: 200, body: 'success' }))
+  return promise
+    .then(() => ({ status: 200, body: 'success' }))
     .catch(() => new Error())
     .finally(() => console.log('Got a response from the API'));
 }
