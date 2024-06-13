71446a1c803430240211e7ab33737fa1975ab5f9
Emmanuel Adinoyi Adaja <emmanueladaja@yahoo.com>
2024-06-13 15:39:08 +0100

    Handling a promise call with then, catch and finally

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,7 @@
+// handling promise
+
+export default function handleResponseFromAPI(promise) {
+  promise.then(() => {return {status: 200, body: 'success'}})
+      .catch(() => {return new Error()})
+      .finally(() => {console.log('Got a response from the API')})
+}
