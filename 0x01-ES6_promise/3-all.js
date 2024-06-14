bfc77d8a681af1ba54dcbc1bd0dc3c907821038e
Emmanuel Adinoyi Adaja <emmanueladaja@yahoo.com>
2024-06-14 06:02:10 +0100

    used Promise.all

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,11 @@
+import { uploadPhoto, createUser } from './utils.js'
+// handle multiple promises
+
+export default function handleProfileSignup() {
+  const calls = [uploadPhoto, createUser];
+  Promise.all(calls.map(call => call()))
+    .then(result => {
+      console.log(`${result[0].body} ${result[1].firstName} ${result[1].lastName}`);
+    })
+    .catch(() => console.log('Signup system offline'));
+}
