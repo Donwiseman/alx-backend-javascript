9420921185edf9c719cf1ba537e0e84330d83487
Emmanuel Adaja <emmanueladaja@yahoo.com>
2024-06-14 02:44:57 -0700

    eslint errors corrected

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,5 +1,5 @@
 // Returns a promise passed user name
 
 export default function signUpUser(firstName, lastName) {
-  return Promise.resolve({firstName: firstName, lastName: lastName});
+  return Promise.resolve({ firstName, lastName });
 }
