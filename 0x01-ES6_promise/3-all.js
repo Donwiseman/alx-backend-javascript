e6f47130f6fd090e9ad5cbda3169a44ab907b3fb
Emmanuel Adaja <emmanueladaja@yahoo.com>
2024-06-13 22:06:24 -0700

    Eslint error correction

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,10 +1,10 @@
-import { uploadPhoto, createUser } from './utils.js'
+import { uploadPhoto, createUser } from './utils';
 // handle multiple promises
 
 export default function handleProfileSignup() {
   const calls = [uploadPhoto, createUser];
-  Promise.all(calls.map(call => call()))
-    .then(result => {
+  Promise.all(calls.map((call) => call()))
+    .then((result) => {
       console.log(`${result[0].body} ${result[1].firstName} ${result[1].lastName}`);
     })
     .catch(() => console.log('Signup system offline'));
