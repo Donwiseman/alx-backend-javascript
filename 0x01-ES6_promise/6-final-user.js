b31bbaec5fb2cc75bf1302ae2f359c408efef36b
Emmanuel Adinoyi Adaja <emmanueladaja@yahoo.com>
2024-06-14 22:29:03 +0100

    adjusted payload

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -5,5 +5,15 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   const signPromise = signUpUser(firstName, lastName);
   const uploadPromise = uploadPhoto(fileName);
   const promises = [signPromise, uploadPromise];
-  return Promise.allSettled(promises).then((result) => result);
+  return Promise.allSettled(promises).then((result) => {
+    const processedResult = result.map((obj) => {
+      if (obj.status === 'fulfilled') {
+        return {status: obj.status, value: obj.value};
+      } else {
+        return {status: obj.status, value: obj.reason};
+      }
+    });
+    console.log(processedResult);
+    return processedResult;
+  });
 }
