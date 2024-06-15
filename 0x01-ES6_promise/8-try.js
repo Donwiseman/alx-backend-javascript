079fa9d872351df9baa47b8c9ab4e98d9f223fe0
Emmanuel Adinoyi Adaja <emmanueladaja@yahoo.com>
2024-06-15 07:53:28 +0100

    Added a simple division function that can throw an zero error

---
+++ 0x01-ES6_promise/8-try.js
@@ -0,0 +1,8 @@
+// a simple division function that throws an error for division by zero
+
+export default function divideFunction(numerator, denominator) {
+  if (denominator === 0) {
+    throw new Error('cannot divide by 0');
+  }
+  return numerator / denominator;
+}
